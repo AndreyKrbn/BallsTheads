d0fd5338381c24cc32c49631fb2a63c53453d817
AndreyKrbn <36628772+AndreyKrbn@users.noreply.github.com>
2018-02-24 05:42:11 +0300

    More balls

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -20,29 +20,43 @@ public class Main extends Application {
         Group root = new Group();
 
         GameField field = new GameField(10,10);
+
         Scene scene = new Scene(root, 50*field.getSizeX(), 50*field.getSizeY(), Color.BLACK);
+
         ReentrantLock locker = new ReentrantLock();
+
         Player p1 = new Player(field, locker);
         Player p2 = new Player(field, locker);
         Player p3 = new Player(field, locker);
         Player p4 = new Player(field, locker);
         Player p5 = new Player(field, locker);
-
+        Player p6 = new Player(field, locker);
+        Player p7 = new Player(field, locker);
+        Player p8 = new Player(field, locker);
+        Player p9 = new Player(field, locker);
 
         root.getChildren().addAll(p1.getBall().getCircle());
         root.getChildren().addAll(p2.getBall().getCircle());
         root.getChildren().addAll(p3.getBall().getCircle());
         root.getChildren().addAll(p4.getBall().getCircle());
         root.getChildren().addAll(p5.getBall().getCircle());
-        primaryStage.setScene(scene);
+        root.getChildren().addAll(p6.getBall().getCircle());
+        root.getChildren().addAll(p7.getBall().getCircle());
+        root.getChildren().addAll(p8.getBall().getCircle());
+        root.getChildren().addAll(p9.getBall().getCircle());
 
+        primaryStage.setScene(scene);
         primaryStage.show();
+
         (new Thread(p1)).start();
         (new Thread(p2)).start();
         (new Thread(p3)).start();
         (new Thread(p4)).start();
         (new Thread(p5)).start();
-
+        (new Thread(p6)).start();
+        (new Thread(p7)).start();
+        (new Thread(p8)).start();
+        (new Thread(p9)).start();
     }
 
 
